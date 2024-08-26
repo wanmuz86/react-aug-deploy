7f2134a47f3690b2480249efb1e87729fa4658bd
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-26 15:55:35 +0800

    explain about jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-    <Header/>
-   <Main/>
-   <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
+    return (
+        <div>
+            <h1 style={{color:'blue',fontSize:'40px'}}>Welcome to my app</h1>
+            <p>I hope you enjoy it</p>
+          
+        </div>
+    )
 }
 
 export default Main
