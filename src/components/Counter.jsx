d7718a9155dd1014cc8acd3cc72012bb52bf0abf
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-26 16:33:21 +0800

    add message behaviour (state)

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,16 +9,35 @@ const Counter = () => {
     // 0 => Initial value
 
     const [counter, setCounter] = useState(0);
+    // Create a variable 
+    // getter = message
+    // setter = setMessage
+    // initial value = ""
+    const [message, setMessage] = useState("");
 
-    const increment = () => setCounter(counter+1);
-    const decrement = () => setCounter(counter - 1);
-    const reset = () => setCounter(0);
+    const increment = () => {
+        setMessage("");
+        setCounter(counter + 1);
+    }
+
+    const decrement = () => {
+        if (counter == 0) {
+            setMessage("Number cannot be less than 0")
+        } else {
+            setCounter(counter - 1)
+        }
+    };
+    const reset = () => {
+        setMessage("");
+        setCounter(0);
+    }
 
     return (
         <div>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
             <button onClick={reset}>Reset</button>
+            <p style={{ color: 'red' }}>{message}</p>
             <h3>Current Count: {counter}</h3>
         </div>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,6 @@ const Main = () => {
         <div>
             <h1 style={{color:'blue',fontSize:'40px'}}>Welcome to my app</h1>
             <p>I hope you enjoy it</p>
-
             <Counter/>
         </div>
     )
