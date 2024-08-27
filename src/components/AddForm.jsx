c18ac545532d0be8cc21bb3be5478594d5530fd5
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-27 11:25:59 +0800

    step 3 and step 4 of forms

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -5,16 +5,31 @@ const AddForm = () => {
         name: '',
         age: ''
     })
+
+    // reference to the event clicked on the caller, e.target here refers to caller (the input)
     const handleNameChanged = (e) => {
-        setFormData({...formData, name:e.target.value})
+        setFormData({...formData, name:e.target.value}) // Object spread operator
     }
 
     const handleAgeChanged = (e) => {
-        setFormData({...formData, name:e.target.value})
+        setFormData({...formData, age:e.target.value}) // Oject spread operator
     }
-    
+
+    // e is the action happening on the form
+    // e.target is the form
+    const handleSubmit = (e) => {
+        e.preventDefault() // Stop the default POST behaviour of browser / don't refresh the page
+        console.log(formData) // to verify data added by user
+
+        setFormData({
+            name:'',
+            age:''
+        })
+
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name</label>
                 <input type="text" id='name' name='name' value={formData.name} 
