f21a7771d1c47018dca537aea5a85375b2e779f7
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-27 10:51:54 +0800

    add map and key

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,23 +3,33 @@ import Counter from './Counter'
 import UserInfo from './UserInfo';
 
 const Main = () => {
+    // object
+    const user = { id: 1, name: "John Doe", age: 25 };
 
-    const user ={id:1, name:"John Doe", age:25};
+    // Array of object
+    const users = [{ id: 1, name: "John Doe", age: 25 }, { id: 2, name: "Alice", age: 16 },
+    { id: 3, name: "Alex Smith", age: 32 }, { id: 4, name: "Jeniffer Doe", age: 30 }]
 
     return (
         <div>
-            <h1 style={{color:'blue',fontSize:'40px'}}>Welcome to my app</h1>
+            <h1 style={{ color: 'blue', fontSize: '40px' }}>Welcome to my app</h1>
             <p>I hope you enjoy it</p>
             <hr />
             <h2>Counter section</h2>
-            <Counter initialValue={0} minValue={0}/>
+            <Counter initialValue={0} minValue={0} />
             <hr />
-            <Counter initialValue={5} minValue={-99999}/>
+            <Counter initialValue={5} minValue={-99999} />
             <hr />
-            <Counter initialValue={10} minValue={7}/>
+            <Counter initialValue={10} minValue={7} />
+
+            <hr />
+            <h2>User List</h2>
+            {
+                users.map(val=><li key={val.id}><UserInfo user={val}/></li>)
+            }
             <hr />
             <h2>User Information</h2>
-            <UserInfo user={user}/>
+            <UserInfo user={user} />
 
         </div>
     )
